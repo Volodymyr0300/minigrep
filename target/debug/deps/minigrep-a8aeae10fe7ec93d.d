f99915e96a956c0f3697edc793f22c2e37bc3baf
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-10-23 20:06:50 -0500

    accepting_command_line_agruments_reading_agr_values

---
+++ target/debug/deps/minigrep-a8aeae10fe7ec93d.d
@@ -0,0 +1,5 @@
+/Users/admin/Documents/rust/minigrep/target/debug/deps/libminigrep-a8aeae10fe7ec93d.rmeta: src/main.rs
+
+/Users/admin/Documents/rust/minigrep/target/debug/deps/minigrep-a8aeae10fe7ec93d.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/minigrep-d1c2def87fe1f6e3.d
@@ -0,0 +1,5 @@
+/Users/admin/Documents/rust/minigrep/target/debug/deps/libminigrep-d1c2def87fe1f6e3.rmeta: src/main.rs
+
+/Users/admin/Documents/rust/minigrep/target/debug/deps/minigrep-d1c2def87fe1f6e3.d: src/main.rs
+
+src/main.rs:

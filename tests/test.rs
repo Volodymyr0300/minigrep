cd1e92a52b5a35a292c2746b6184336d3d09edb6
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-10-25 10:03:43 -0500

    working_with_enviroment_variables_case_sensitive_and_insensitive

--- tests/test.rs
+++ tests/test.rs
@@ -1,4 +1,4 @@
-use minigrep::{Config, run, search};
+use minigrep::{Config, run, search, search_case_insensitive};
 use std::{fs, error::Error};
 
 #[test]
@@ -11,6 +11,7 @@ fn test_run_reads_file() -> Result<(), Box<dyn Error>> {
     let config = Config {
         query: String::from("test"),
         filename: filename.to_string(),
+        case_sensitive: true,
     };
 
     let result = run(config);
@@ -37,15 +38,31 @@ fn test_config_new_with_valid_args() {
 }
 
 #[test]
-fn one_result() {
+fn case_sensitive() {
     let query = "duct";
     let contents = "\
         Rust:
         safe, fast, productive.
-        Pick three.";
+        Pick three.
+        Duct tape.";
     
     assert_eq!(
         vec!["safe, fast, productive."],
         search(query, contents)
     );
+}
+
+#[test]
+fn case_insensitive() {
+    let query = "rUsT";
+    let contents = "\
+        Rust:
+        safe, fast, productive.
+        Pick three.
+        Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+);
 }
